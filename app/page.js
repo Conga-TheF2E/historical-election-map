cc2db37c52725bd6975af22c0d076caa5bed99d8
wu <66793170+pat2304107@users.noreply.github.com>
2023-11-26 22:48:31 +0800

    feat: 忘了加回來了

--- app/page.js
+++ app/page.js
@@ -40,7 +40,7 @@ export default function Home() {
 
   return (
     <main className="overflow-hidden bg-gray100 font-GenSekiGothic-R">
-      {/* <Map screenLevel={screenLevel} enteredSecondPage={enteredSecondPage} /> */}
+      <Map screenLevel={screenLevel} enteredSecondPage={enteredSecondPage} />
       <Index
         enteredSecondPage={enteredSecondPage}
         setEnteredSecondPage={setEnteredSecondPage}
