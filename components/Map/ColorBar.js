f72499661a0a95bd02534d9ec1639e426d6a2aa2
wu <66793170+pat2304107@users.noreply.github.com>
2023-11-28 21:47:57 +0800

    記憶卡

--- components/Map/ColorBar.js
+++ components/Map/ColorBar.js
@@ -8,19 +8,20 @@ export default function RenderColorBar({
   return (
     <section
       className={`bottom-0 mx-auto w-[348px] md:w-[768px] xl:w-[1320px] ${
-        !enteredSecondPage || (!selectedCity && mapMode !== "common")
-          ? "hidden"
-          : "relative"
+        !enteredSecondPage || selectedCity ? "hidden" : "relative"
       }`}
     >
+      <div className="h-0">1</div>
       {screenLevel && (
         <img
           className={`absolute bottom-[19px] md:bottom-[50px] xl:bottom-9 ${
             screenLevel == "xl"
-              ? "left-[332px]"
-              : screenLevel == "md"
-                ? "left-[476px]"
-                : "left-5"
+              ? "left-8"
+              : screenLevel == "sm"
+                ? "left-5"
+                : mapMode == "common"
+                  ? "left-9"
+                  : "left-[476px]"
           }`}
           src={`/img/colorbar_${screenLevel}_${mapMode}.svg`}
           alt="colorBar"
